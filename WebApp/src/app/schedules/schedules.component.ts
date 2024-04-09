65060982156145e996eeb67a82e86c6bb6b70bef
anonymous0904 <farcauadda@gmail.com>
2024-04-09 17:20:18 +0300

    Lists

--- WebApp/src/app/schedules/schedules.component.ts
+++ WebApp/src/app/schedules/schedules.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 import { Schedule } from '../schedule';
-import { Time } from '@angular/common';
+import { NgIf, Time, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SCHEDULES } from '../mock-schedules';
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
 
 @Component({
   selector: 'app-schedules',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,NgFor,NgIf,UpperCasePipe],
   templateUrl: './schedules.component.html',
   styleUrl: './schedules.component.css'
 })
 export class SchedulesComponent {
-  schedule: Schedule = {
-    id: 1,
-    description: 'Wedding',
-    type: 'Event',
-    date: new Date('2021-09-25'),
-    startTime: { hours: 12, minutes: 0 },
-    endTime: { hours: 13, minutes: 0 },
-    username: 'Maria'
-  };
+  schedules=SCHEDULES;
+  selectedSchedule?: Schedule;
+  onSelect(schedule: Schedule): void {
+    this.selectedSchedule = schedule;
+  }
 }
