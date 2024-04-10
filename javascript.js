5eebfb991065510c0f252805465ecb877060d765
anonymous0904 <farcauadda@gmail.com>
2024-04-10 16:28:35 +0300

    dayGridMonth full calendar

---
+++ javascript.js
@@ -0,0 +1,13 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
+
